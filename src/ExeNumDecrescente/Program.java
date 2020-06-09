d2fc8b5799c8e175b0ab662f24832f2933e78979
Lucas Alvarenga <lucas.souza.alvarenga@hotmail.com>
2020-06-09 10:16:26 -0300

    Atualizando Exe Decrescente

--- src/ExeNumDecrescente/Program.java
+++ src/ExeNumDecrescente/Program.java
@@ -1,3 +1,7 @@
+/*Crie uma classe java NumeroDecrescente que contenha um método que receba
+um número inteiro e imprima, em ordem decrescente, o valor do número até
+0.
+ */
 package ExeNumDecrescente;
 
 import java.util.Scanner;
